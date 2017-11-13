64fe9581f680161a81a55f5c43845d78b5f5280b
lorenzstortz <lorenzstortz@outlook.de>
2017-11-13 15:19:09 +0100

    First Commit

---
+++ src/main/java/Gruppe07/BreakfastToTheLimit/App.java
@@ -0,0 +1,13 @@
+package Gruppe07.BreakfastToTheLimit;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
