bc7d1fcae941693b6a1dd852af1a4de1a1f166fe
lorenzstortz <lorenzstortz@outlook.de>
2017-11-13 16:02:35 +0100

    Changed pom

--- src/main/java/Gruppe07/BreakfastToTheLimit/App.java
+++ src/main/java/Gruppe07/BreakfastToTheLimit/App.java
@@ -1,13 +1,15 @@
 package Gruppe07.BreakfastToTheLimit;
 
-import com.google.gson.Gson;
-
 /**
- * Hello world!
+ * @author sieber, stortz
  *
  */
 public class App {
+
+  private static final String GOOGLE_API_KEY = "AIzaSyAd40gDCh2eFTHVr6ocvmyElOf7J93BYrQ";
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
+
 }
---
+++ src/main/java/Gruppe07/BreakfastToTheLimit/model/Roommate.java
@@ -0,0 +1,9 @@
+package Gruppe07.BreakfastToTheLimit.model;
+
+public class Roommate {
+  //
+  // private String destination;
+  //
+  // public Roommate(String destination, )
+
+}
